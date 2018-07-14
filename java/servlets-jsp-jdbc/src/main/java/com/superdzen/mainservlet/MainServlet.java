7286ba9b76d1865829de955cf41f75ccf20d1e14
superdzen <superdzen@gmail.com>
2018-07-14 19:27:57 +0700

    Mapping changed

--- java/servlets-jsp-jdbc/src/main/java/com/superdzen/mainservlet/MainServlet.java
+++ java/servlets-jsp-jdbc/src/main/java/com/superdzen/mainservlet/MainServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/")
+@WebServlet("/home")
 public class MainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
