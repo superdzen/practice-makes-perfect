d73145b09fda2eda6efc60e57e1748e592a8e499
superdzen <superdzen@gmail.com>
2018-08-19 11:15:06 +0700

    Added new module for practicing Spring annotations

---
+++ java/spring-demo-annotations/src/main/java/com/superdzen/springdemo/AnnotationDemoApp.java
@@ -0,0 +1,21 @@
+package com.superdzen.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+    public static void main(String[] args) {
+
+        // read Spring config file
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // get the bean from Spring container
+        Coach theCoach = context.getBean("thatSillyCoach", Coach.class);
+
+        // call a method on the bean
+        System.out.println(theCoach.getDailyWorkout());
+
+        // close the context
+        context.close();
+    }
+}
---
+++ java/spring-demo-annotations/src/main/java/com/superdzen/springdemo/TennisCoach.java
@@ -0,0 +1,12 @@
+package com.superdzen.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component("thatSillyCoach")
+public class TennisCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your backhand volley.";
+    }
+}
