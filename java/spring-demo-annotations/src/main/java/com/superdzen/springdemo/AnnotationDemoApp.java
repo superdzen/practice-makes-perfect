bc33c72e249da29904a5690b6e97d546cf7938b3
superdzen <superdzen@gmail.com>
2018-08-19 12:08:52 +0700

    Added another Coach implementation as Component

--- java/spring-demo-annotations/src/main/java/com/superdzen/springdemo/AnnotationDemoApp.java
+++ java/spring-demo-annotations/src/main/java/com/superdzen/springdemo/AnnotationDemoApp.java
@@ -9,11 +9,13 @@ public class AnnotationDemoApp {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        // get the bean from Spring container
-        Coach theCoach = context.getBean("thatSillyCoach", Coach.class);
-
+        // get beans from Spring container
+        Coach theCoach = context.getBean("tennisCoach", Coach.class);
+        Coach thePingPongCoach = context.getBean("pingPongCoach", Coach.class);
         // call a method on the bean
-        System.out.println(theCoach.getDailyWorkout());
+        System.out.println("Tennis coach: " + theCoach.getDailyWorkout());
+        System.out.println("Tennis coach: " + theCoach.getDailyFortune());
+        System.out.println("Ping-Pong coach: " + thePingPongCoach.getDailyWorkout());
 
         // close the context
         context.close();
