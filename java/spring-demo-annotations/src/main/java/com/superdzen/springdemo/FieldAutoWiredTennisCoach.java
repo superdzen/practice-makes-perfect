e2396cb2011a8042b0d098517d7a3820dbb0f77c
superdzen <superdzen@gmail.com>
2018-08-21 20:02:35 +0700

    Added Bean scope example via annotations

--- java/spring-demo-annotations/src/main/java/com/superdzen/springdemo/FieldAutoWiredTennisCoach.java
+++ java/spring-demo-annotations/src/main/java/com/superdzen/springdemo/FieldAutoWiredTennisCoach.java
@@ -2,9 +2,11 @@ package com.superdzen.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class FieldAutoWiredTennisCoach implements Coach {
 
     @Autowired
