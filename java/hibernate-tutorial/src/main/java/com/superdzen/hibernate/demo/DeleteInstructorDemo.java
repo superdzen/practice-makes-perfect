5d921535fc3efe7e197d080c2f47193ce9e5781a
superdzen <superdzen@gmail.com>
2018-09-01 22:29:32 +0700

    Testing cascade deleting

---
+++ java/hibernate-tutorial/src/main/java/com/superdzen/hibernate/demo/DeleteInstructorDemo.java
@@ -0,0 +1,46 @@
+package com.superdzen.hibernate.demo;
+
+import com.superdzen.hibernate.demo.entity.onetoone.Instructor;
+import com.superdzen.hibernate.demo.entity.onetoone.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class DeleteInstructorDemo {
+
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate_one_to_one.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+
+            session.beginTransaction();
+
+            // get instructor by PK
+            int id = 1;
+
+            Instructor tmpInstructor = session.get(Instructor.class, id);
+
+            // delete the instructor
+
+            if (tmpInstructor != null) {
+                System.out.println("Found instructor: " + tmpInstructor);
+                System.out.println("Deleting...");
+                session.delete(tmpInstructor);
+            }
+
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
+    }
+
+
+}
