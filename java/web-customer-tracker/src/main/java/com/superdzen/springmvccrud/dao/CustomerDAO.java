cecc5dbe6215118a88ef686302d2a3c14ec6883b
superdzen <superdzen@gmail.com>
2018-09-16 11:10:32 +0700

    Added simple CustomerDAO implementation

---
+++ java/web-customer-tracker/src/main/java/com/superdzen/springmvccrud/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.superdzen.springmvccrud.dao;
+
+import com.superdzen.springmvccrud.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerDAO {
+
+    public List<Customer> getCustomers();
+
+}
