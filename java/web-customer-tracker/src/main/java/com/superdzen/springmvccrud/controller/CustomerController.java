f4d6d95aca6accbb2f6667b9ff771e0bfed7dba5
superdzen <superdzen@gmail.com>
2018-09-16 11:19:29 +0700

    Added implementation for listCustomers using CustomerDAO

--- java/web-customer-tracker/src/main/java/com/superdzen/springmvccrud/controller/CustomerController.java
+++ java/web-customer-tracker/src/main/java/com/superdzen/springmvccrud/controller/CustomerController.java
@@ -1,15 +1,31 @@
 package com.superdzen.springmvccrud.controller;
 
+import com.superdzen.springmvccrud.dao.CustomerDAO;
+import com.superdzen.springmvccrud.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+    //inject the customer DAO
+    @Autowired
+    private CustomerDAO customerDAO;
+
+
     @RequestMapping("/list")
     public String listCustomers(Model model) {
+
+        // get customer form the DAO
+        List<Customer> customers = customerDAO.getCustomers();
+
+        // add the list of customers to the model
+        model.addAttribute("customers", customers);
 
         return "list-customers";
     }
