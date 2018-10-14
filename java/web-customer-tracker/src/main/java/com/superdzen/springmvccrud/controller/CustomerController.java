500c9ff01d74ed21be0f54eb5aea0e4e119b5577
superdzen <superdzen@gmail.com>
2018-10-14 20:22:09 +0700

    Changed from RequestMapping to GetMapping

--- java/web-customer-tracker/src/main/java/com/superdzen/springmvccrud/controller/CustomerController.java
+++ java/web-customer-tracker/src/main/java/com/superdzen/springmvccrud/controller/CustomerController.java
@@ -5,6 +5,7 @@ import com.superdzen.springmvccrud.entity.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -18,7 +19,7 @@ public class CustomerController {
     private CustomerDAO customerDAO;
 
 
-    @RequestMapping("/list")
+    @GetMapping("/list")
     public String listCustomers(Model model) {
 
         // get customer form the DAO
---
+++ java/web-customer-tracker/src/main/java/com/superdzen/springmvccrud/service/CustomerService.java
@@ -0,0 +1,9 @@
+package com.superdzen.springmvccrud.service;
+
+import com.superdzen.springmvccrud.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    public List<Customer> getCustomers();
+}
