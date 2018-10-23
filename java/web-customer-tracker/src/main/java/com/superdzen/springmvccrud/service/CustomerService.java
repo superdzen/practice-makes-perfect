3e63dbf434020298f4b1d9ae2f10ca1ddb61f2d1
superdzen <superdzen@gmail.com>
2018-10-23 21:26:56 +0700

    Added saveCustomer method

--- java/web-customer-tracker/src/main/java/com/superdzen/springmvccrud/service/CustomerService.java
+++ java/web-customer-tracker/src/main/java/com/superdzen/springmvccrud/service/CustomerService.java
@@ -5,5 +5,7 @@ import com.superdzen.springmvccrud.entity.Customer;
 import java.util.List;
 
 public interface CustomerService {
-    public List<Customer> getCustomers();
+    List<Customer> getCustomers();
+
+    void saveCustomer(Customer customer);
 }
