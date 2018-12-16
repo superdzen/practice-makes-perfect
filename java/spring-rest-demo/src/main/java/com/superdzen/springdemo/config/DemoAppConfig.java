ad16a2b4ac04ad5b409287e52e3e896f9195420d
Denis Kushnerik <superdzen@gmail.com>
2018-12-16 13:36:42 +0700

    Added new module for rest demo app

---
+++ java/spring-rest-demo/src/main/java/com/superdzen/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,11 @@
+package com.superdzen.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.superdzen.springdemo")
+public class DemoAppConfig {
+}
---
+++ java/spring-rest-demo/src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Welcome Page</title>
+</head>
+<body>
+Spring Rest demo
+<hr/>
+<a href="${pageContext.request.contextPath}/test/hello">Link to Hello World page</a>
+</body>
+</html>
