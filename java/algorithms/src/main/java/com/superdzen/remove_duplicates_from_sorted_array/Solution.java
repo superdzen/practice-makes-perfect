27e175356680d87741af043b33aa985de394c117
Denis Kushnerik <superdzen@gmail.com>
2019-09-08 12:59:16 +0700

    Solution for "Remove duplicates from sorted array"

---
+++ java/algorithms/src/main/java/com/superdzen/remove_duplicates_from_sorted_array/Solution.java
@@ -0,0 +1,24 @@
+package com.superdzen.remove_duplicates_from_sorted_array;
+
+/**
+ * Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ */
+public class Solution {
+    private static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        System.out.println(removeDuplicates(arr));
+    }
+}
