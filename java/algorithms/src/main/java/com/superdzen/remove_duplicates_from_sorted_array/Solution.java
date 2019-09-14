1c7148ac0db1b7701819338d9dac58405b61928d
Denis Kushnerik <superdzen@gmail.com>
2019-09-14 12:22:49 +0700

    Added message

--- java/algorithms/src/main/java/com/superdzen/remove_duplicates_from_sorted_array/Solution.java
+++ java/algorithms/src/main/java/com/superdzen/remove_duplicates_from_sorted_array/Solution.java
@@ -19,6 +19,6 @@ public class Solution {
 
     public static void main(String[] args) {
         int[] arr = new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
-        System.out.println(removeDuplicates(arr));
+        System.out.println("The answer is: " + removeDuplicates(arr)); // 5
     }
 }
